3a1d1e3fd401a0b21f0474bc73032a3d02932223
M S I <M S I@MSI>
2023-12-06 18:29:35 +0700

    add container to class aplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,10 @@
 package com.example.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repositori.ContainerApp
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+
 }
