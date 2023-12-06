76a47c9e8783b813be9930175a4484eae2224bd0
M S I <M S I@MSI>
2023-12-06 18:15:15 +0700

    add compain object

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel(){
+    companion object{
+        private const val  TIMEOUT_MILLIS = 5_000L
+    }
 
 
 }
