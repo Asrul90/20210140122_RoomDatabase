a2be5eef6881811d6ffdcb7fa434bd7f5f6832e7
M S I <M S I@MSI>
2023-12-06 18:31:43 +0700

    add containerDataAPP to class aplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -2,13 +2,14 @@ package com.example.roomsiswa
 
 import android.app.Application
 import com.example.roomsiswa.repositori.ContainerApp
+import com.example.roomsiswa.repositori.ContainerDataApp
 
 class AplikasiSiswa : Application() {
     lateinit var container: ContainerApp
 
     override fun onCreate() {
         super.onCreate()
-        container = ContainerApp(this)
+        container = ContainerDataApp(this)
     }
 
 
