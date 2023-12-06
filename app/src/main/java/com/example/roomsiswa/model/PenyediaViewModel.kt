4de7676abb3d9ff63059837bb2ae20fee31bfa46
M S I <M S I@MSI>
2023-12-06 18:38:30 +0700

    add fun aplikasi Siswa

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.roomsiswa.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+
+
+    }
+
+}
+
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
